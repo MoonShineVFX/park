a106bc7f06f516879401e2881e10953bf72932ad
David Lai <davidlatwe@gmail.com>
2022-01-20 21:23:36 +0800

    add 'root_scope_suite'

--- src/allzpark/rezplugins/command/rezconfig.py
+++ src/allzpark/rezplugins/command/rezconfig.py
@@ -4,4 +4,7 @@ from allzpark import util
 
 park = {
     "suite_root": util.normpath("~/rez/sweet/release"),
+
+    # a suite for the scope where no project has been selected
+    "root_scope_suite": "",
 }

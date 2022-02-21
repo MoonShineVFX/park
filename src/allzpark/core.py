31598cd06708c23ca1d677c0e63d988a21ed20a4
David Lai <davidlatwe@gmail.com>
2022-02-21 18:31:17 +0800

    remove unused

--- src/allzpark/core.py
+++ src/allzpark/core.py
@@ -384,14 +384,3 @@ def re_resolve_rxt(context):
         package_orderers=rxt.package_orderers,
         building=rxt.building,
     )
-
-
-def make_shell_tool(scope):
-    name = "<shell>"
-    return SuiteTool(
-        name=name,
-        alias=name,
-        ctx_name="",
-        variant=None,
-        scope=scope,
-    )
